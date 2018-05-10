3dc779fef5d748fe9b88042632b90cad6381c191
Piotr Bielak <piotrbielak2@gmail.com>
2018-05-10 23:58:30 +0200

    Agent + Env + Game + GUI

--- rl_ttt/agents.py
+++ rl_ttt/agents.py
@@ -1,22 +1,45 @@
 """
 Agent implementations
 """
+import numpy as np
 from rl import core
+
+from rl_ttt.game import FieldStates
 
 
 class QLearningAgent(core.Agent):
     compiled = True
 
-    def __init__(self):
+    def __init__(self, gui_callback):
         super(QLearningAgent, self).__init__()
+
+        self.gui_callback = gui_callback
 
     def forward(self, observation):
         # TODO
-        pass
+        from time import sleep
+        sleep(1)
+
+        board, _, _ = observation
+        empty_idxs = [idx for idx, field in enumerate(board) if field == FieldStates.EMPTY_FIELD]
+
+        if not empty_idxs:
+            return -1
+
+        return np.random.choice(empty_idxs)
 
     def backward(self, reward, terminal):
         # TODO
-        pass
+
+        fmt_str = 'Received reward: {}\n' \
+                  'Terminal: {}'
+
+        if self.gui_callback:
+            self.gui_callback(None, fmt_str.format(reward, terminal))
+        else:
+            print('Received reward:', reward)
+            print('Terminal', terminal)
+            print('-' * 30)
 
     def load_weights(self, filepath):
         return
--- rl_ttt/environment.py
+++ rl_ttt/environment.py
@@ -3,28 +3,72 @@ RL environment
 """
 from rl import core
 
+from rl_ttt import game
+
 
 class TicTacToeEnv(core.Env):
 
-    def __init__(self):
+    def __init__(self, gui_callback):
         self.nb_step = 0
+        self.game = game.TicTacToe()
+        self.gui_callback = gui_callback
 
     def step(self, action):
         self.nb_step += 1
+        field_idx = action
 
-        # TODO
-        observation = (1,)
-        reward = 0
-        done = False
+        if field_idx >= 0:
+            if self.game.is_field_empty(field_idx):
+                self.game.set_field(field_idx, self.game.current_player)
+
+                if self.game.is_terminal():
+                    reward = 1 if self.game.has_won()[1] == game.FieldStates.X_MARKER else -1
+                else:
+                    reward = 0
+
+            else:
+                reward = -1
+        else:
+            if self.game.is_terminal():
+                reward = 1 if self.game.has_won()[
+                                  1] == game.FieldStates.X_MARKER else -1
+            else:
+                reward = 0
+
+        # TODO: convert observation to proper format (!)
+        observation = (self.game.board,
+                       self.game.is_terminal(),
+                       self.game.has_won())
+
+        done = self.game.is_terminal()
         info = {}
+
         return observation, reward, done, info
 
     def reset(self):
-        # TODO
-        return self.step(None)
+        self.game.reset()
+        return (self.game.board,
+                self.game.is_terminal(),
+                self.game.has_won())
 
     def render(self, mode='human', close=False):
-        print('Current step:', self.nb_step)
+        fmt_str = 'Current step: {}\n' \
+                  'Game: {}\n' \
+                  'Current player: {}\n' \
+                  'Is terminal: {} | Won:{}'
+
+        msg = fmt_str.format(
+            self.nb_step,
+            list(map(lambda x: x.value, self.game.board)),
+            self.game.current_player.value,
+            self.game.is_terminal(),
+            self.game.has_won()
+        )
+
+        if self.gui_callback:
+            self.gui_callback(self.game.board, msg)
+        else:
+            print(msg)
 
     def seed(self, seed=None):
         return
